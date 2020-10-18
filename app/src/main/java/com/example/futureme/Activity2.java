a5e9fc40bd2adc4f5be299ddfc89e1782acb3ec7
msun8 <msun8@ncsu.edu>
2020-10-17 22:16:35 -0400

    edit the register pages

--- app/src/main/java/com/example/futureme/Activity2.java
+++ app/src/main/java/com/example/futureme/Activity2.java
@@ -2,12 +2,22 @@ package com.example.futureme;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
 
+//    EditText editTextName;
+//    TextView name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+//
+//        editTextName = findViewById(R.id.editName);
+//        name = findViewById(R.id.name);
+//
+//        bt
+
     }
 }
